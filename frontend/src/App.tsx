4a0772ebc4dcda48e6875f24b374e082f5650e40
TitanEkanayake <titanekanayake9@gmail.com>
2024-03-12 22:54:02 +0530

    front end development update and Created hooks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,60 @@
+import { useState } from "react";
 import "./App.css";
+import { CompactPicker } from "react-color";
+import { EthosConnectStatus, SignInButton, ethos } from "ethos-connect";
 
 function App() {
-  return (
-    <div
-      style={{ display: "flex", justifyContent: "center", marginTop: "4rem" }}
-    >
-      <h1>el 1</h1>
-      <h1>el 2</h1>
-    </div>
-  );
+  const [color, set_color] = useState("#00000)");
+  const { status, wallet } = ethos.useWallet();
+
+  console.log(wallet?.contents);
+
+  if (status == EthosConnectStatus.Connected) {
+    return (
+      <div
+        style={{ display: "flex", justifyContent: "center", marginTop: "4rem" }}
+      >
+        <div
+          style={{ border: "1px solid black", width: "800px", height: "800px" }}
+        ></div>
+        <div
+          style={{
+            margin: "4rem",
+            width: "512px",
+            lineHeight: "2rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+          }}
+        >
+          <h1> Sui Place</h1>
+          <p style={{ marginTop: "4rem", marginBottom: " 4rem" }}>
+            Are you ready to join the sui community in placing a tile on the
+            board? Select a color then ctrl+click (win) or cmd+click (mac) to
+            place.
+          </p>
+          <div style={{ marginBottom: "4rem" }}>
+            <CompactPicker
+              color={color}
+              onChangeComplete={(new_color) => set_color(new_color.hex)}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  } else if (status == EthosConnectStatus.Loading) {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <h1 style={{ marginTop: "16rem" }}>Loading Wallet...</h1>
+      </div>
+    );
+  } else {
+    return (
+      <div style={{ display: "flex", justifyContent: "center" }}>
+        <SignInButton></SignInButton>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ frontend/src/utils/place_pixels.ts
@@ -0,0 +1,20 @@
+import { TransactionBlock } from "@mysten/sui.js/transactions";
+import { PACKAGE_ID, PLACE_ID } from "../deployed_objects.json";
+
+export const get_set_pixel_trx = (
+  x: number,
+  y: number,
+  color: number
+): TransactionBlock => {
+  const trx = new TransactionBlock();
+  trx.moveCall({
+    target: `${PACKAGE_ID}::board::set_pixel_at`,
+    arguments: [
+      trx.object(PLACE_ID),
+      trx.pure(x),
+      trx.pure(y),
+      trx.pure(color),
+    ],
+  });
+  return trx;
+};
